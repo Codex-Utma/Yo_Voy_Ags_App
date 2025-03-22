b7eafdb458038b0fc56747117bbeacecfa4e4cb3
Carlos Galindo <utm22030587@utma.edu.mx>
2025-03-21 18:18:39 -0600

    feat: set up initial layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,1 +1,58 @@
-// aqui va lo del permiso de la camara y la ubicaciongit
+// import { Auth0Provider} from 'react-native-auth0';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { useCameraPermissions } from "expo-camera";
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function RootLayout() {
+
+  const [permission, setPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return (
+      <View style={styles.centeredContainer}>
+        <Text>No permission</Text>
+      </View>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={setPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  return (
+    // <Auth0Provider domain={`${process.env.EXPO_PUBLIC_AUTH0_DOMAIN}`} clientId={`${process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID}`}>
+      <SafeAreaProvider>
+        <StatusBar />
+        <Stack
+          screenOptions={{
+            headerShown: false
+          }}
+        />
+      </SafeAreaProvider>
+    // </Auth0Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
